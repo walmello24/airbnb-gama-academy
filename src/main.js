9017ac5c907b5031d0106bfdaee21e796463e327
Walter Melo <walmello24@gmail.com>
2020-05-23 15:44:34 -0300

    finish

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-// Faremos as requisações em uma API externa, cujo endpoint já está listado abaixo.
+ // Faremos as requisações em uma API externa, cujo endpoint já está listado abaixo.
 const API_URL = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";
 
 // Para isso, usaremos o "fetch", conforme você deve ter visto nas aulas online.
 // Corrija a linha abaixo, para que a requisição funcione.
-XMLHttpRequest(API_URI)
+fetch(API_URL)
   .then(response => response.text())
   .then(result => {
     // Aqui, estamos pegando o resultado da requisição e parseando em JSON, 
@@ -11,45 +11,65 @@ XMLHttpRequest(API_URI)
     const data = JSON.parse(result);
 
     // Como é mesmo que percorremos um array? 'hm
-    dados.map(element => {
+    data.map(element => {
       // Desestruture seu objeto, trazendo os seguintes dados:
       // Foto, tipo, nome e preço da propriedade.
-      const { variavel, chave } = data;
+      const { photo, name, property_type, price } = element;
 
       // Pegue a linha do container onde serão exibidos os cards.
-      row = document.pega("linha");
+      row = document.querySelector("#row");
 
       // Agora, crie um elemento "div" com a class "col-md-4" do Bootstrap.
-      div = document.criaElemento("div");
-      div.nomeDaClasse = "col-md-4";
+      div = document.createElement("div");
+      div.className = "col-md-4";
 
       // Boa! Agora, precisamos criar a "div" do card a ser exibido. Ao criar, atribua a class "card mb-4 box-shadow".
-      card = ?
+      card = document.createElement("div")
+      card.className = "card mb-4 box-shadow"
+      div.appendChild(card)
 
       // Humm... Agora que já temos o card, que tal começarmos a popular as informações?
       // Crie um elemento "img" e, atribua a class como "card-img-top";
       // Não se esqueça que sua imagem precisa ser referenciada!
+      image = document.createElement("img")
       image.className = "card-img-top";
-      image.referencia = minhaImagem;
+      image.src = photo;
+      card.appendChild(image)
 
       // Legal! Agora, já podemos começar a construir o "corpo" do card. Bora lá!
       // Obs.: atribua a class "card-body" do Bootstrap, para que fique bonitão. :)
-      cardBody = ?
+      cardBody = document.createElement("div")
+      cardBody.className = "card-body"
+      card.appendChild(cardBody)
 
       // Beleza, corpo criado. Seguindo nossa estrutura, o próximo elemento seria os textos, para exibir
       // o tipo da propriedade, nome e preço. Mas, antes, vamos criar um elemento "div" para manter isso
       // tudo organizado. Você pode atribuir a class "card-text" do Bootstrap!
-      cardText = ?
+      cardText = document.createElement("div")
+      cardText.className = "card-text"
+      cardBody.appendChild(cardText)
 
       // Uffa! Finalmente os elementos da proriedade!
       // Precisamos agora, listar o tipo, o nome e o preço.
-      propertyType = criaParagrafo("p");
+      propertyType = document.createElement("p");
       propertyType.className = "property-type";
-      propertyType.innerHTML = element;
+      propertyType.innerHTML = property_type
+      cardText.appendChild(propertyType)
 
-      propertyName = ?
+      // Uffa! Finalmente os elementos da proriedade!
+      // Precisamos agora, listar o tipo, o nome e o preço.
+      propertyType = document.createElement("p");
+      propertyType.className = "property-name";
+      propertyType.innerHTML = name
+      cardText.appendChild(propertyType)
 
-      propertyPrice = ?
+            // Uffa! Finalmente os elementos da proriedade!
+      // Precisamos agora, listar o tipo, o nome e o preço.
+      propertyType = document.createElement("p");
+      propertyType.className = "property-price";
+      propertyType.innerHTML = `Total de R$ ${price},00.`
+      cardText.appendChild(propertyType)
+
 
       // AOBA! Temos agora, todos os elementos necessários para percorrer a resposta da API e,
       // anexar um ao outro para exibição ao usuário. Para "anexar" os elementos, nós usaremos
